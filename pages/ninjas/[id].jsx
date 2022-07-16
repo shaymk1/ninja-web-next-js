c3b069e1c1154484ecc864a7f1dcd09eff05ed01
Mamosebetsi Kekae <mkekae@gmail.com>
2022-07-16 14:55:57 -0700

    completed id.jsx, ninjas.jsx, jobs.module.css, ninja.module.css

---
+++ pages/ninjas/[id].jsx
@@ -0,0 +1,44 @@
+
+{
+	/*getStaticPaths tells next js how many html pages it needs to made based on our data first*/
+}
+
+export const getStaticPaths = async () => {
+	const res = await fetch("https://jsonplaceholder.typicode.com/users");
+	const data = await res.json();
+
+	// map data to an array of path objects with params (id)
+	const paths = data.map((ninja) => {
+		return {
+			params: { id: ninja.id.toString() },
+		};
+	});
+
+	return {
+		paths,
+		fallback: false,
+	};
+};
+
+export const getStaticProps = async (context) => {
+	const id = context.params.id;
+	const res = await fetch("https://jsonplaceholder.typicode.com/users/" + id);
+	const data = await res.json();
+
+	return {
+		props: { ninja: data },
+	};
+};
+
+const Details = ({ ninja }) => {
+	return (
+		<div>
+			<h1>{ninja.name}</h1>
+			<p>{ninja.email}</p>
+			<p>{ninja.website}</p>
+			<p>{ninja.address.city}</p>
+		</div>
+	);
+};
+
+export default Details;
--- pages/ninjas/index.jsx
+++ pages/ninjas/index.jsx
@@ -1,5 +1,5 @@
-import React from 'react'
-
+import styles from "../../styles/Jobs.module.css";
+import Link from "next/Link";
 
 export const getStaticProps = async () => {
 	const res = await fetch("https://jsonplaceholder.typicode.com/users");
@@ -11,11 +11,19 @@ export const getStaticProps = async () => {
 };
 
 const Ninjas = ({ ninjas }) => {
-  console.log(ninjas);
-	return <div>
-   
-   <h1>All Ninjas</h1>
-   </div>;
+	console.log(ninjas);
+	return (
+		<div>
+			<h1>All Ninjas</h1>
+			{ninjas.map((ninja) => (
+				<Link href ={'/ninjas/' + ninja.id} key={ninja.id}>
+					<a className={styles.single}>
+						<h3>{ninja.name}</h3>
+					</a>
+				</Link>
+			))}
+		</div>
+	);
 };
 
 export default Ninjas;
